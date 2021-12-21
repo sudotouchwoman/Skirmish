c1d1666709665effedbb1bc9eaf5f10026874c34
ArtemPushPop <artemiygotovtsev@gmail.com>
2021-12-21 13:15:02 +0300

    commit with done work.

--- project/graphics.cpp
+++
@@ -1,7 +0,0 @@
-#include "manager.hpp"
-
-int main (int argc, char *argv[]) {
-    GameManager manager;
-    manager.Run(5001);
-    return 0;
-}
--- project/graphics_1.cpp
+++
@@ -1,7 +0,0 @@
-#include "manager.hpp"
-
-int main (int argc, char *argv[]) {
-    GameManager manager;
-    manager.Run(5002);
-    return 0;
-}
